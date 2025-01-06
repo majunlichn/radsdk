f9e8e5ca22cfb3a1f604eee893613142d59c67a7
Jerry Ma <majunlichn@outlook.com>
2025-01-06 18:36:27 +0800

    update the libraries

--- README.md
+++ README.md
@@ -1,32 +1,37 @@
 # radsdk
 
-A software development kit contains common C/C++ packages, easy to setup and use.
+radsdk is a software development kit that contains common and popular C/C++ libraries.
+
+radsdk is easy to setup and use. The repository provides build scripts to download, build and install libraries, with prebuilt binaries to save you time.
 
 # Usage
 
-Setup [microsoft/vcpkg](https://github.com/microsoft/vcpkg):
+First, setup [microsoft/vcpkg](https://github.com/microsoft/vcpkg):
 
-    ```powershell
-    git clone https://github.com/microsoft/vcpkg.git
-    # Run the bootstrap script:
-    cd vcpkg
-    .\bootstrap-vcpkg.bat # Linux: ./bootstrap-vcpkg.sh
-    # Configure the VCPKG_ROOT environment variable:
-    $env:VCPKG_ROOT="C:\path\to\vcpkg" # Linux: export VCPKG_ROOT="/path/to/vcpkg"
-    ```
+```powershell
+git clone https://github.com/microsoft/vcpkg.git
+# Run the bootstrap script:
+cd vcpkg
+.\bootstrap-vcpkg.bat # Linux: ./bootstrap-vcpkg.sh
+# Configure the VCPKG_ROOT environment variable for convenience:
+$env:VCPKG_ROOT="C:\path\to\vcpkg" # Linux: export VCPKG_ROOT="/path/to/vcpkg"
+```
 
-Download and build additional libraries:
+Then download and build the libraries with the python script `setup.py`:
+
 ```powershell
 cd radsdk
-# Configure CMake generator:
+# Configure CMake generator with the following environment variables:
 $env:CMAKE_GENERATOR="Visual Studio 17 2022"
 $env:CMAKE_GENERATOR_PLATFORM="x64"
-# Execute setup.py:
+# Execute setup.py to download, build and install the libraries:
 python setup.py
 # Configure the RADSDK_ROOT environment variable for convenience:
 $env:RADSDK_ROOT="C:\path\to\radsdk" # Linux: export RADSDK_ROOT="/path/to/radsdk"
 ```
-Generate project files for CMake projects that dependent on vcpkg packages with manifest mode:
+
+To use the vcpkg libraries you need to configure `VCPKG_MANIFEST_DIR` and `VCPKG_INSTALLED_DIR`:
+
 ```powershell
-cmake -S . -B build -D VCPKG_MANIFEST_DIR=$env:RADSDK_ROOT -D VCPKG_INSTALLED_DIR=$env:RADSDK_ROOT/vcpkg_installed
+cmake -S . -B build -D VCPKG_MANIFEST_DIR="$env:RADSDK_ROOT" -D VCPKG_INSTALLED_DIR="$env:RADSDK_ROOT/vcpkg_installed"
 ```
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ import shutil
 import urllib.request
 from zipfile import ZipFile
 
-working_dir_before_execute = os.getcwd()
+working_dir_before = os.getcwd()
 script_root = os.path.dirname(os.path.realpath(__file__))
 working_dir_stack = list()
-platform_name = None
+triplet = None
 
 def chdir(path: str):
     os.chdir(path)
@@ -39,12 +39,13 @@ def download_file(url, filename):
     print(f"Downloaded {filename}")
 
 def extract_zip(filename, path="."):
+    print(f"Extracting {filename} ...")
     with ZipFile(filename, "r") as zip:
         zip.extractall(path)
+    print(f"Extracted {filename}")
 
 def build_SDL():
-    print(f"Build Platform: {platform_name}")
-    chdir(script_root + "/" + platform_name)
+    chdir(script_root + "/" + triplet)
     if not os.path.exists("SDL"):
         run("git clone https://github.com/libsdl-org/SDL.git")
     chdir("SDL")
@@ -55,47 +56,49 @@ def build_SDL():
     run(f"cmake --build build --target install --config Release")
 
 def build_SDL_mixer():
-    chdir(script_root + "/" + platform_name)
+    chdir(script_root + "/" + triplet)
     if not os.path.exists("SDL_mixer"):
         run("git clone https://github.com/libsdl-org/SDL_mixer.git")
     chdir("SDL_mixer")
     run("git checkout 90859376266adcd602499e94e0ac0c10fb55f712")
     run("git submodule update --init --recursive")
     install_dir = os.getcwd() + "/installed"
-    sdl3_dir = script_root + f"/{platform_name}/SDL/installed"
+    sdl3_dir = script_root + f"/{triplet}/SDL/installed"
     run(f"cmake -S . -B build -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=\"{install_dir}\"",
                    env=dict(os.environ, SDL3_DIR=sdl3_dir))
     run(f"cmake --build build --target install --config Release")
 
 def main() -> int:
     try:
-        global platform_name
+        global triplet
         if platform.machine() == "AMD64":
             if platform.system() == "Windows":
-                platform_name = "x64-windows"
+                triplet = "x64-windows"
             if platform.system() == "Linux":
-                platform_name = "x64-linux"
+                triplet = "x64-linux"
         if platform is None:
             print("Cannot determine the target platform!")
             return 0
-        if not os.path.exists(platform_name):
-            os.mkdir(platform_name)
+        if not os.path.exists(triplet):
+            os.mkdir(triplet)
 
-        print(f"Build Platform: {platform_name}")
+        print(f"Target Triplet: {triplet}")
 
         chdir(script_root)
+        run("vcpkg install")
         build_SDL()
         build_SDL_mixer()
 
         chdir(script_root)
-        chdir(platform_name)
-        download_file("https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-9.1.0-winx64.zip", "mysql-connector-c++-9.1.0-winx64.zip")
-        extract_zip("mysql-connector-c++-9.1.0-winx64.zip")
-        chdir(working_dir_before_execute)
+        if triplet == "x64-windows":
+            chdir(triplet)
+            download_file("https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-9.1.0-winx64.zip", "mysql-connector-c++-9.1.0-winx64.zip")
+            extract_zip("mysql-connector-c++-9.1.0-winx64.zip")
+        chdir(working_dir_before)
         return 0
     except Exception as e:
         print(e)
-        chdir(working_dir_before_execute)
+        chdir(working_dir_before)
         return -1
 
 if __name__ == '__main__':
